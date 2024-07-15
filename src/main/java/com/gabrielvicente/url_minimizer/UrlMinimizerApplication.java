c004d16684562ef6376ab28801475d6cc7e6bc34
gabriel.vicente <gabrielvf64@gmail.com>
2024-07-15 11:03:19 -0300

    initial commit

---
+++ src/main/java/com/gabrielvicente/url_minimizer/UrlMinimizerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.url_minimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlMinimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlMinimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielvicente/url_minimizer/UrlMinimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.url_minimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlMinimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
